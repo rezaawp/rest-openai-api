2d6683c0a08b80eb0edb09f0d385691afdfb3843
rezaawp <rezaaawp@gmail.com>
2025-11-24 10:17:14 +0700

    ubah database default ke sqllite

--- config/config.py
+++ config/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://root:password@localhost:5432/openai_api_project")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///mydatabase.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.getenv("INVOICE_DIR", "uploads")
