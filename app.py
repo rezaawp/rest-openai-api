7e3b1556fb42a8a3af64fe333dcfa935e7bd33b9
rezaawp <rezaaawp@gmail.com>
2025-11-24 09:37:03 +0700

    fix(app): api hanya boleh di akses di LAN

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config.config import Config
@@ -6,12 +6,42 @@ import os
 from controllers.upload_controller import upload_bp
 from controllers.invoice_controller import invoice_handler
 from extensions import db
+from controllers.ai_video_controller import ai_video_handler, ai_video_callback_handler
 
 migrate = Migrate()
+
+def is_local_request():
+    ip = request.headers.get("X-Forwarded-For", request.remote_addr)
+
+    # API access only from LAN IP ranges
+    allowed_prefix = (
+        "127.",
+        "192.168.",
+        "10.",
+        "172.16.", "172.17.", "172.18.", "172.19.",
+        "172.20.", "172.21.", "172.22.", "172.23.",
+        "172.24.", "172.25.", "172.26.", "172.27.",
+        "172.28.", "172.29.", "172.30.", "172.31.",
+    )
+
+    return ip.startswith(allowed_prefix)
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Restrict API access to LAN only
+    @app.before_request
+    def restrict_api():
+        # allow static content for public
+        if request.path.startswith("/static"):
+            return
+
+        # restrict only API routes
+        if request.path.startswith("/api"):
+            if not is_local_request():
+                abort(403, description="API only accessible from local network.")
 
     # Initialize extensions
     db.init_app(app)
@@ -20,10 +50,12 @@ def create_app():
     # Register blueprints
     app.register_blueprint(upload_bp, url_prefix="/api")
     app.register_blueprint(invoice_handler, url_prefix="/api")
+    app.register_blueprint(ai_video_handler, url_prefix="/api")
+    app.register_blueprint(ai_video_callback_handler, url_prefix="/callback")
 
     @app.route('/')
     def home():
-        return "Invoice Management API running"
+        return "OpenAI API is running"
 
     return app
 
@@ -36,5 +68,5 @@ if __name__ == '__main__':
     if (os.getenv("ENVIRONMENT") == "development"):
         print("Creating database tables...")
         with app.app_context():
-            db.create_all()  # buat tabel langsung
+            db.create_all()
     app.run(debug=debug, port=port)
